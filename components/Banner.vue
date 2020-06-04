bbaba2fe6fa8011c22cfbc317d75732aaba378a9
Tim <34155548+TotallyTheTim@users.noreply.github.com>
2020-06-04 21:39:23 +0200

    banner start

---
+++ components/Banner.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="banner">
+    <img src="@/static/sbclogo.png" align="Sailboat Companion logo" />
+  </div>
+</template>
+
+<style lang="scss">
+.banner {
+  height: 400px;
+}
+</style>
